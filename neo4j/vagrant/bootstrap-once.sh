2c7200d790c1528f8c33098d368b5eded74a210f
Tom Van den Bulck <ToVn@administrators-MacBook-Pro.local>
2015-09-02 23:07:35 +0200

    updated vagrant so it will no longer ask for confirmation when installing neo4j

--- neo4j/vagrant/bootstrap-once.sh
+++ neo4j/vagrant/bootstrap-once.sh
@@ -4,14 +4,14 @@
 sudo apt-get update
  
 # install java
-sudo apt-get install openjdk-7-jre-headless -y
+sudo apt-get -y install openjdk-7-jre-headless -y
 
 #install neo4j
 sudo -i
 wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
 apt-get update
-apt-get install neo4j
+sudo apt-get -y install neo4j
 
 sudo cp /vagrant/neo4j-server.properties /etc/neo4j/neo4j-server.properties
 sudo service neo4j-service restart
